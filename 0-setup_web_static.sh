4aaebd808bd9dbedd8208244f1906c299ada4199
Montodel <elnenysuleiman@gmail.com>
2023-04-09 12:40:43 -0700

    Fixing the main files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
-
-# sets up your web servers for the deployment of web_static
+#A Bash script that sets up my web servers for the deployment of web_static
 
 #Install Nginx
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
 #Create folders
-sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
 #create a fake HTNL file
-echo "Holberton School for the win!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-#removing the file to modify it
-sudo rm -rf /data/web_static/current
+echo "Holberton School for the win!" >/data/web_static/releases/test/index.html
 
 #Create a symbolic link
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf "/data/web_static/releases/test" "/data/web_static/current"
 
 #give ownership to ubuntu user And Group
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu "/data"
 
-#Update the Nginx configuration to serve the content
-NEW_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "38i $NEW_STRING" /etc/nginx/sites-available/default
+#Update the Nginx configuration to serve the content 
+sudo sed -i "s/^.*location \/hbtn_static.*//" /etc/nginx/sites-available/default
+sudo sed -i \
+	"s/^}$/\tlocation \/hbtn_static \{ alias \/data\/web_static\/current\/; \}\n\}/" \
+	/etc/nginx/sites-available/default
+
+#enable default site
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 #restart Ngnix server
 sudo service nginx restart
